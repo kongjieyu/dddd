7e169b8b88427be9d680728c6c84fa4d659e335d
May Kong <1285235080@qq.com>
2019-01-14 16:46:16 +1300

    modify the Theme of Antd

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Button} from 'antd-mobile'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      board: ['Eric', 'May', 'Panpan']
+    }
+  }
+
+  render(){
+    const mainCareer = 'Design'
+    return(
+      <div>
+        <h1>May Career, /n,Main: {mainCareer}</h1>
+        <h2>Board Member:</h2>
+        {this.state.board.map(v=>{
+          return <li key={v}>{v}</li>
+        })}
+
+        <h3>Other Department:</h3>
+        <Tech Leader='John'/>
+        <Illustration Leader='Cathy'></Illustration>
       </div>
-    );
+    )
+
+  }
+}
+function Illustration(props){
+  return <h3>Leader: {props.Leader}; Illustration</h3>
+}
+
+
+class Tech extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      member: ['dada','xx','yiwai']
+    }
+    this.addMember = this.addMember.bind(this)
+  }
+  componentWillMount(){
+    console.log('Init')
+  }
+  componentDidMount(){
+    console.log('Did')
+  }
+  componentWillReceiveProps(){
+    console.log('Receive Props')
+  }
+  addMember(){
+    console.log('Add new Member')
+    this.setState ({
+      member: [...this.state.member, 'NewMem'+ Math.random()]
+    })
+  }
+  render(){
+    console.log('Doing')
+    const field= 'react, Vues, Angular'
+    
+    return (
+      <div>
+        <h3>Leader: {this.props.Leader}; Web Design : {field} </h3>
+        {this.state.member.map(v=>{return <li key={v}>{v}</li>})}
+        <Button type="primary" onClick={this.addMember}>Add Member</Button>
+      </div>
+    )
+
   }
 }
 
-export default App;
+
+export default App
+
